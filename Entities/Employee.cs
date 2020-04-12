dded8726ae95a366a7952a3bbb70d27704f68a23
YugandharGT <tyugandhar158@outlook.com>
2020-04-12 19:28:22 +0530

    Updated entity
    Added validations

--- Entities/Employee.cs
+++ Entities/Employee.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities
@@ -7,8 +8,13 @@ namespace Entities
     public class Employee
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Email { get; set; }
+        [Required]
         public string Department { get; set; }
     }
 }
+
