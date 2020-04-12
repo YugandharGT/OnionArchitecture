e607d6ebe4f154ba3100ce1f4bb8bce91df15b8c
YugandharGT <tyugandhar158@outlook.com>
2020-04-12 19:34:18 +0530

    New Interface in the module
    Asynchronous Interfaces

--- Interface/IEmployeeTaskRepository.cs
+++ Interface/IEmployeeTaskRepository.cs
@@ -1,1 +1,23 @@
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Interface
+{
+    public interface IEmployeeTaskRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync();
+
+        Task<Employee> GetByIdAsync(int id);
+
+        Task<Employee> InsertAsync(Employee entity);
+
+        Task<Employee> UpdateAsync(Employee entity);
+
+        Task<Employee> RemoveAsync(int id);
+    }
+}
 
