7c32975c6dd6b021d04763a42ceb373597a017f4
TYugandhar <tyugandhar158@outlook.com>
2021-03-29 21:52:41 +0530

    cleaned up the styles

---
+++ BlazorServerApp/Pages/EmailHistoryBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerApp.Pages
+{
+    public class EmailHistoryBase : ComponentBase
+    {
+        public void SubmitAll()
+        {
+
+        }
+
+        public void ReleaseAll()
+        {
+
+        }
+    }
+}
