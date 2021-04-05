94c6d5b135365eb805d415827d630cd495357259
TYugandhar <tyugandhar158@outlook.com>
2021-04-05 17:35:14 +0530

    changed to mudblazor component

---
+++ BlazorServerApp/Theme/MudBlazorCustomTheme.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MudBlazor;
+
+namespace BlazorServerApp.Theme
+{
+    public class MudBlazorCustomTheme
+    {
+        public MudTheme GetLightMode()
+        {
+            return new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Black = "#272c34"
+                }
+            };
+        }
+
+        public MudTheme GetDarkMode()
+        {
+            return new MudTheme()
+            {
+                Palette = new Palette()
+                {
+                    Black = "#27272f",
+                    Background = "#32333d",
+                    BackgroundGrey = "#27272f",
+                    Surface = "#373740",
+                    DrawerBackground = "#27272f",
+                    DrawerText = "rgba(255,255,255, 0.50)",
+                    DrawerIcon = "rgba(255,255,255, 0.50)",
+                    AppbarBackground = "#27272f",
+                    AppbarText = "rgba(255,255,255, 0.70)",
+                    TextPrimary = "rgba(255,255,255, 0.70)",
+                    TextSecondary = "rgba(255,255,255, 0.50)",
+                    ActionDefault = "#adadb1",
+                    ActionDisabled = "rgba(255,255,255, 0.26)",
+                    ActionDisabledBackground = "rgba(255,255,255, 0.12)"
+                }
+
+            };
+        }
+    }
+}
