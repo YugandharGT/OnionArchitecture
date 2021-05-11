3ffe9b9db5effec923923941727e4d3432400f41
TYugandhar <tyugandhar158@outlook.com>
2021-05-11 16:55:06 +0530

    Added login page

---
+++ BlazorServerApp/Pages/LoginBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorServerApp.Pages
+{
+    public class LoginBase : ComponentBase
+    {
+
+        [Parameter]
+        public string UserName { get; set; }
+        [Parameter]
+        public string Password { get; set; }
+        [Parameter]
+        public bool RememberMe { get; set; } = true;
+
+        public void SignIn()
+        {
+            //NavigationManager navigation = new ;
+        }
+    }
+}
